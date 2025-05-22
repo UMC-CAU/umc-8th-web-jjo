43c481756fa1d73a5ed62b44484b5ff46f42ceb9
hyun-wjd <helencho0530@gmail.com>
2025-05-22 20:52:38 +0900

    오류 해결

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,61 @@
-import './App.css';
+import "./App.css";
 
-import {createBrowserRouter, RouterProvider} from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import ErrorPage from './pages/ErrorPage';
-import LoginPage from './pages/LoginPage';
-import SignupPage from './pages/SignupPage';
-import HomeLayout from './layouts/HomeLayout';
-import MyPage from './pages/MyPage';
-import { AuthProvider } from './context/AuthContext';
-import ProtectedLayout from './layouts/ProtectedLayout';
-import { RouteObject } from 'react-router-dom';
-
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import ErrorPage from "./pages/ErrorPage";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+import HomeLayout from "./layouts/HomeLayout";
+import MyPage from "./pages/MyPage";
+import { AuthProvider } from "./context/AuthContext";
+import ProtectedLayout from "./layouts/ProtectedLayout";
+import { RouteObject } from "react-router-dom";
 
 //publicRoutes: 인증 없이 접근 가능한 라우트
-const publicRoutes = RouteObject[] = [
-  {
-    path: '/',
-    element: <HomeLayout />,
-    errorElement: <ErrorPage />,
-  children: [
+const publicRoutes: RouteObject[] = [
     {
-      index: true, element: <HomePage />
+        path: "/",
+        element: <HomeLayout />,
+        errorElement: <ErrorPage />,
+        children: [
+            {
+                index: true,
+                element: <HomePage />,
+            },
+            {
+                path: "login",
+                element: <LoginPage />,
+            },
+            {
+                path: "signup",
+                element: <SignupPage />,
+            },
+        ],
     },
-    {
-      path: 'login', element: <LoginPage />
-    },
-    {
-      path: "signup",
-      element: <SignupPage />
-    },
-  ],
-},
 ];
 
 //privateRoutes: 인증이 필요한 라우트
-const protectedRoutes = RouteObject[] = [
-  {
-    path:"/",
-    element: <ProtectedLayout />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        path: "my",
-        element: <MyPage />,
-      },
-    ],
-
-  },
+const protectedRoutes: RouteObject[] = [
+    {
+        path: "/",
+        element: <ProtectedLayout />,
+        errorElement: <ErrorPage />,
+        children: [
+            {
+                path: "my",
+                element: <MyPage />,
+            },
+        ],
+    },
 ];
 
-const router = createBrowserRouter([...publicRoutes]);
+const router = createBrowserRouter([...publicRoutes, ...protectedRoutes]);
 
 function App() {
-  return (
-  <AuthProvider>
-    <RouterProvider router={router} />
-    </AuthProvider>
-  );
+    return (
+        <AuthProvider>
+            <RouterProvider router={router} />
+        </AuthProvider>
+    );
 }
 export default App;
